7649aca3b1011626e791841e60d287014d3bda0f
JaroslawJaniak <JaroslawJaniak>
2024-08-04 08:57:17 +0200

    style form components

--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -4,78 +4,153 @@ import { STATUS_OPTIONS, StatusOption } from "../models/StatusOption";
 import { useState } from "react";
 
 export function AddPage() {
-    const navigate = useNavigate();
-    const [statusOptions] = useState<StatusOption[]>(STATUS_OPTIONS);
+  const navigate = useNavigate();
+  const [statusOptions] = useState<StatusOption[]>(STATUS_OPTIONS);
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-        event.preventDefault();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
 
-        const form = event.target as HTMLFormElement;
-        const formData = new FormData(form);
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
 
-        const data: any = {};
+    const data: any = {};
 
-        formData.forEach((value, key) => {
-            data[key] = value;
-        })
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
 
-        createEmployee(data).then(() => {
-            navigate('/');
-        });
-    }
+    createEmployee(data).then(() => {
+      navigate("/");
+    });
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h1 className="pt-4 pb-4">Add Employee</h1>
-            <div className="row mb-3">
-                <div className="col">
-                    <label htmlFor="firstname" className="form-label">Firstname</label>
-                    <input className="form-control" type="text" name="firstname" required />
-                </div>
-                <div className="col">
-                    <label htmlFor="lastname" className="form-label">Lastname</label>
-                    <input className="form-control" type="text" name="lastname" required/>
-                </div>
-                <div className="col">
-                    <label htmlFor="birthdate" className="form-label">Birthdate</label>
-                    <input className="form-control" type="date" name="birthdate" required />
-                </div>
-            </div>
-            <div className="row mb-3">
-                <div className="col">
-                <label htmlFor="phonenumber" className="form-label">Phonenumber</label>
-                <input className="form-control" type="text" name="phonenumber" required />
-                </div>
-            </div>
-            <div className="row mb-3">
-                <div className="col">
-                    <label htmlFor="address" className="form-label">Address</label>
-                    <input type="text" className="form-control" name="address" required />
-                </div>
-                <div className="col">
-                    <label htmlFor="city" className="form-label">City</label>
-                    <input type="text" className="form-control" name="city" required />
-                </div>
-                <div className="col">
-                    <label htmlFor="postalcode" className="form-label">Postal Code</label>
-                    <input type="text" className="form-control" name="postalcode" required />
-                </div>
-            </div>
-            <div className="row mb-3">
-                <div className="col">
-                    <label htmlFor="status" className="form-label">Status</label>
-                    <select className="form-control" name="status">
-                        {statusOptions.map((status) => (<option value={status.value}>{status.label}</option>))}
-                    </select>
-                </div>
-                <div className="col">
-                    <label htmlFor="salary" className="form-label">Salary</label>
-                    <input type="text" className="form-control" name="salary" required />
-                </div>
-            </div>
-            <div className="row">
-                <button className="btn btn-primary" type="submit">Add</button>
-            </div>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit} className="mx-12">
+      <h1 className="pt-4 pb-4">Add Employee</h1>
+      <hr className="my-5" />
+      <div className="sm:grid grid-cols-3 mb-3 ">
+        <div className="col">
+          <label htmlFor="firstname" className="form-label">
+            Firstname
+          </label>
+          <input
+            className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            type="text"
+            name="firstname"
+            required
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="lastname" className="form-label">
+            Lastname
+          </label>
+          <input
+            className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            type="text"
+            name="lastname"
+            required
+          />
+        </div>
+      </div>
+
+      <div className="sm:grid grid-cols-3 mb-3">
+        <div className="col">
+          <label htmlFor="birthdate" className="form-label">
+            Birthdate
+          </label>
+          <input
+            className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            type="date"
+            name="birthdate"
+            required
+          />
+        </div>
+        <div className="row mb-3">
+          <div className="col">
+            <label htmlFor="phonenumber" className="form-label">
+              Phonenumber
+            </label>
+            <input
+              className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+              type="text"
+              name="phonenumber"
+              required
+            />
+          </div>
+        </div>
+      </div>
+      <hr className="my-5" />
+      <div className="sm:grid grid-cols-3 mb-3">
+        <div className="col">
+          <label htmlFor="address" className="form-label">
+            Address
+          </label>
+          <input
+            type="text"
+            className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            name="address"
+            required
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="city" className="form-label">
+            City
+          </label>
+          <input
+            type="text"
+            className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            name="city"
+            required
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="postalcode" className="form-label">
+            Postal Code
+          </label>
+          <input
+            type="text"
+            className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            name="postalcode"
+            required
+          />
+        </div>
+      </div>
+      <hr className="my-5" />
+      <div className="sm:grid grid-cols-3 w-100">
+        <div className="col">
+          <label htmlFor="status" className="form-label">
+            Status
+          </label>
+          <select
+            className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            name="status"
+          >
+            {statusOptions.map((status) => (
+              <option value={status.value}>{status.label}</option>
+            ))}
+          </select>
+        </div>
+        <div className="col">
+          <label htmlFor="salary" className="form-label">
+            Salary
+          </label>
+          <input
+            type="text"
+            className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+            name="salary"
+            required
+          />
+        </div>
+      </div>
+      <hr className="my-5" />
+      <div className="grid grid-cols-6 gap-4">
+        <button
+          className="col-start-6 col-end-7 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 font-medium rounded text-sm px-5 py-2.5  mb-2 w-24 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          type="submit"
+        >
+          Add
+        </button>
+      </div>
+    </form>
+  );
 }
--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -25,66 +25,151 @@ export function DetailsPage() {
     }
 
     return (
-        <>
-            <div className="d-flex justify-content-between align-items-center">
-                <h1 className="pt-4 pb-4">Detaile Page</h1>
-                <button onClick={handleEditClick} className="btn btn-warning">Edit</button>
-            </div>
-     
-            { data ? 
-                <section>
-                            <div className="row mb-3">
-                <div className="col">
-                    <label htmlFor="firstname" className="form-label">Firstname</label>
-                    <input className="form-control" type="text" id="firstname" value={data.firstname} readOnly />
-                </div>
-                <div className="col">
-                    <label htmlFor="lastname" className="form-label">Lastname</label>
-                    <input className="form-control" type="text" id="lastname" value={data.lastname} readOnly />
-                </div>
-                <div className="col">
-                    <label htmlFor="birthdate" className="form-label">Birthdate</label>
-                    <input className="form-control" type="text" id="birthdate" value={data.birthdate.toDateString()} readOnly />
-                </div>
-            </div>
-            <div className="row mb-3">
-                <div className="col">
-                <label htmlFor="phonenumber" className="form-label">Phonenumber</label>
-                <input className="form-control" type="text" id="phonenumber" value={data.phonenumber} readOnly />
-                </div>
-            </div>
-            <div className="row mb-3">
-                <div className="col">
-                    <label htmlFor="address" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="address" value={data.address} readOnly />
-                </div>
-                <div className="col">
-                    <label htmlFor="city" className="form-label">City</label>
-                    <input type="text" className="form-control" id="city" value={data.city} readOnly />
-                </div>
-                <div className="col">
-                    <label htmlFor="postalcode" className="form-label">Postal Code</label>
-                    <input type="text" className="form-control" id="postalcode" value={data.postalcode} readOnly />
-                </div>
-            </div>
-            <div className="row mb-3">
-                <div className="col">
-                    <label htmlFor="id" className="form-label">ID</label>
-                    <input type="text" className="form-control" id="id" value={data.id} readOnly />
-                </div>
-                <div className="col">
-                    <label htmlFor="status" className="form-label">Status</label>
-                    <input type="text" className="form-control" id="status" value={data.status} readOnly />
-                </div>
-                <div className="col">
-                    <label htmlFor="salary" className="form-label">Salary</label>
-                    <input type="text" className="form-control" id="salary" value={data.salary} readOnly />
-                </div>
-            </div>
-                </section> : ''
+      <>
+        <h1 className="uppercase">Detaile Page</h1>
 
-            }
-
-        </>
-    )
+        {data ? (
+          <section>
+            <div className="sm:grid grid-cols-3 mb-3 ">
+              <div className="col">
+                <label htmlFor="firstname" className="form-label">
+                  Firstname
+                </label>
+                <input
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  type="text"
+                  id="firstname"
+                  value={data.firstname}
+                  readOnly
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="lastname" className="form-label">
+                  Lastname
+                </label>
+                <input
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  type="text"
+                  id="lastname"
+                  value={data.lastname}
+                  readOnly
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="birthdate" className="form-label">
+                  Birthdate
+                </label>
+                <input
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  type="text"
+                  id="birthdate"
+                  value={data.birthdate.toDateString()}
+                  readOnly
+                />
+              </div>
+            </div>
+            <div className="sm:grid grid-cols-3 mb-3 ">
+              <div className="col">
+                <label htmlFor="id" className="form-label">
+                  ID
+                </label>
+                <input
+                  type="text"
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  id="id"
+                  value={data.id}
+                  readOnly
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="phonenumber" className="form-label">
+                  Phonenumber
+                </label>
+                <input
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  type="text"
+                  id="phonenumber"
+                  value={data.phonenumber}
+                  readOnly
+                />
+              </div>
+            </div>
+            <div className="sm:grid grid-cols-3 mb-3 ">
+              <div className="col">
+                <label htmlFor="address" className="form-label">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  id="address"
+                  value={data.address}
+                  readOnly
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="city" className="form-label">
+                  City
+                </label>
+                <input
+                  type="text"
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  id="city"
+                  value={data.city}
+                  readOnly
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="postalcode" className="form-label">
+                  Postal Code
+                </label>
+                <input
+                  type="text"
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  id="postalcode"
+                  value={data.postalcode}
+                  readOnly
+                />
+              </div>
+            </div>
+            <div className="sm:grid grid-cols-3 mb-3 ">
+              <div className="col">
+                <label htmlFor="status" className="form-label">
+                  Status
+                </label>
+                <input
+                  type="text"
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  id="status"
+                  value={data.status}
+                  readOnly
+                />
+              </div>
+              <div className="col">
+                <label htmlFor="salary" className="form-label">
+                  Salary
+                </label>
+                <input
+                  type="text"
+                  className="block pt-1 pb-1 ps-10  text-gray-900 border border-gray-300 rounded w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+                  id="salary"
+                  value={data.salary}
+                  readOnly
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-6 gap-4">
+              <button
+                onClick={handleEditClick}
+                className="col-start-6 col-end-7 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 font-medium rounded text-sm px-5 py-2.5  mb-2 w-24 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              >
+                Edit
+              </button>
+            </div>
+          </section>
+        ) : (
+          ""
+        )}
+      </>
+    );
 }
