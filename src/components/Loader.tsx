7ac5429f3b4c52dcd1701f377328006097329853
JaroslawJaniak <JaroslawJaniak>
2024-08-12 12:05:32 +0200

    loader style changed to fixed

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,20 +2,22 @@
 
 export const Loader = () => {
   return (
-    <div className="w-fit mx-auto absolute top-1/2 left-1/2">
-      <div className="lds-spinner">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="fixed w-full top-1/3 left-0">
+      <div className="w-fit mx-auto">
+        <div className="lds-spinner">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
