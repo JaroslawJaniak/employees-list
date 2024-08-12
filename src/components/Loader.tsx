49ecbc779fe9f961ad61a2dc55bd591ddd8e1a44
JaroslawJaniak <JaroslawJaniak>
2024-08-12 11:53:22 +0200

    cypress & loader style

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@
 
 export const Loader = () => {
   return (
-    <div className="w-fit mx-auto">
+    <div className="w-fit mx-auto absolute top-1/4 left-1/2">
       <div className="lds-spinner">
         <div></div>
         <div></div>
